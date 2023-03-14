2c59caf42dcd717c04b85128706954c003e4e1b5
Hongsin <ainghongsin9999@gmail.com>
2023-03-14 23:12:00 +0700

    Complated TP&

--- TP7/Ex1/index.js
+++ TP7/Ex1/index.js
@@ -3,9 +3,6 @@ const path = require('path');
 const fs = require("fs")
 
 const app = express()
-
-const filePath = path.join(__dirname, 'public', 'index.html');
-
  
 app.get('/', function (req, res) {
     fs.readFile('./src/Index.html', 'utf8', (err, data) => {
@@ -13,18 +10,12 @@ app.get('/', function (req, res) {
             console.error(err)
             return;
         }
-        
         fs.readFile('./src/styles.css', 'utf8', (err, cssData) => {
             const updatedData = data.replace('<link rel="stylesheet" href="./styles.css">', `<style>${cssData}</style>`);
 
             res.send(updatedData)
         });
-        
-
     })
-
-    console.log(__dirname)
-
 })
 
 // start the server
